2c56fa1b260478743890165b0f94179ab8277e2e
lenasz01 <lenazhng@gmail.com>
2026-08-23 18:33:36 -0500

    creating first get and post requests

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def root():
     return {"Hello" : "World!"}
+
+# uvicorn main:app --reload
+# how to post: curl -X POST -H "Content-Type: application/json" "http://127.0.0.1:8000/items?item=apple"
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
