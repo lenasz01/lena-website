cc9d49491599196c6dbb3365bc668dd7bbbe38a4
lenasz01 <lenazhng@gmail.com>
2026-08-25 21:23:21 -0500

    create model object

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# This is a Model object
+class Item(BaseModel):
+    text: str
+    is_done: bool = False
 
 items = []
 
@@ -10,21 +16,22 @@ def root():
 
 # uvicorn main:app --reload
 
-# how to post: curl -X POST -H "Content-Type: application/json" "http://127.0.0.1:8000/items?item=apple"
+# when item was str: curl -X POST -H "Content-Type: application/json" "http://127.0.0.1:8000/items?item=apple"
+# when item is a Model object: curl -X POST -H "Content-Type: application/json" -d '{"text": "apple","is_done": false}' "http://127.0.0.1:8000/items"
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
-# how to get: curl -X GET http://127.0.0.1:8000/items/0
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+# curl -X GET http://127.0.0.1:8000/items?limit=3
+@app.get("/items", response_model=list[Item])
+def get_item(limit: int = 10):
+    return items[0:limit]
+
+# curl -X GET http://127.0.0.1:8000/items/0
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
         raise HTTPException(status_code=404, detail="Item not found")
-
-
-
-    item = items[item_id]
-    return item
